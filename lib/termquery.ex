1329986d194b9ac87ef25d5c0384066f1d724ee6
Vorce <joel@vorce.se>
2015-05-18 14:32:42 +0200

    TermQuery supports multiple fields

--- lib/termquery.ex
+++ lib/termquery.ex
@@ -1,10 +1,10 @@
 defmodule TermQuery do
-  defstruct field: :id, value: nil, type: :term
-  @type t :: %TermQuery{field: Atom.t, value: any, type: Atom.t}
+  defstruct fields: [], value: nil, type: :term
+  @type t :: %TermQuery{fields: List.t, value: any, type: Atom.t}
 
   defimpl Search, for: TermQuery do
     def search(query, indices) do
-      IO.puts("Searching for term '#{query.value}' in field '#{query.field}', in indices '#{indices}'")
+      IO.puts("Searching for term '#{query.value}' in fields '#{fields(query.fields)}', in indices '#{indices}'")
       hits = indices |> Enum.flat_map(&term_in(&1, query))
       %Result{ hits: hits }
     end
@@ -13,15 +13,22 @@ defmodule TermQuery do
       Indexstore.get(index).terms
         |> Map.get(query.value, %{}) # Map with docIds as keys
         |> Enum.filter(fn({_id, locs}) ->
-          on_field(locs, query.field) != [] end) # Only hits with correct field
+          on_fields(locs, query.fields) != [] end) # Only hits with correct fields
         |> Enum.into(%{})
         |> Map.keys # TODO we disregard positions/highlights for now
         |> Enum.map(&Documentstore.get(&1))
     end
 
-    def on_field(locs, field) do
+    def on_fields(locs, []) do locs end
+    def on_fields(locs, fields) do
       locs
-        |> Enum.filter(fn(%Location{} = l) -> l.field == field end)
+        |> Enum.filter(fn(%Location{} = l) ->
+          Enum.member?(fields, l.field) end)
+    end
+
+    def fields([]) do "*" end
+    def fields(fs) do
+      Enum.join(fs, ", ")
     end
   end
 end
--- test/cedrik_test.exs
+++ test/cedrik_test.exs
@@ -31,25 +31,42 @@ defmodule CedrikTest do
     Indexstore.put(idx)
     result = Search.search(%MatchAll{}, [idx.name])
     assert length(result.hits) == 1
-    assert result.hits |> Enum.map(fn(d) -> d.id end) |> Enum.member?(0)
+    assert result.hits |> ids |> Enum.member?(0)
   end
 
   test "search for specific term" do
-    result = Search.search(%TermQuery{field: :title, value: "Pojke"}, ["test-index"])
+    result = Search.search(%TermQuery{fields: [:title], value: "Pojke"}, ["test-index"])
     assert length(result.hits) == 1
     assert hd(result.hits).id == 3
   end
 
   test "search for term with multiple hits" do
-    result = Search.search(%TermQuery{field: :body, value: "att"},
+    result = Search.search(%TermQuery{fields: [:body], value: "att"},
       ["foo", "test-index"])
     assert length(result.hits) > 1
-    assert result.hits |> Enum.map(fn(d) -> d.id end) |> Enum.sort ==
+    assert result.hits |> ids |> Enum.sort ==
       [0, 1, 2]
   end
 
   test "term query obeys field parameter" do
-    result = Search.search(%TermQuery{field: :body, value: "cedrik"}, ["test-index"])
-    assert result.hits |> Enum.map(fn(d) -> d.id end) == [42]
+    result = Search.search(%TermQuery{fields: [:body], value: "cedrik"},
+      ["test-index"])
+    assert result.hits |> ids == [42]
+  end
+
+  test "term query on many fields" do
+    r = Search.search(%TermQuery{fields: [:title, :body], value: "cedrik"},
+      ["test-index"])
+    assert r.hits |> ids |> Enum.sort == [42, 666]
+  end
+
+  test "term query without fields looks at all" do
+    r = Search.search(%TermQuery{fields: [], value: "cedrik"},
+      ["test-index"])
+    assert r.hits |> ids |> Enum.sort == [42, 666]
+  end
+
+  def ids(hits) do
+    hits |> Enum.map(fn(d) -> d.id end)
   end
 end
