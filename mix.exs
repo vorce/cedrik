b9b505f5e4d39c1c6e5fe69dcf47744a66a12b4d
Joel C <joel@vorce.se>
2017-10-06 21:22:11 +0200

    Upgrade to elixir 1.5

--- mix.exs
+++ mix.exs
@@ -4,35 +4,23 @@ defmodule Cedrik.Mixfile do
   def project do
     [app: :cedrik,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [mod: {Cedrik, []},
      applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:chronos, "~> 1.7"},
      {:exredis, "~> 0.2"},
      {:poison, "~> 2.2"},
 
-     {:credo, "~> 0.6", only: [:dev, :test]},
+     {:credo, "~> 0.8", only: [:dev, :test]},
      {:inch_ex, "~> 0.5", only: [:dev, :test]}
    ]
   end
